e031719db09cae03c2558002b9751e0936af05b9
RzenoLabs <150924936+RzenoLabs@users.noreply.github.com>
2023-11-27 13:38:04 +0000

    UPDATE CONST

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x796fDcDf49963b066ccB8B54cD11A0fA1B88F8b1";
+  "0xd25CCF3cf4c6f18A91743518bd10bd1DCaaF8C89";
 export const tokenContractAddress =
-  "0x5F2c89E950499D0f4409399f095c8CcAd49745d6";
+  "0x62F704F2B7F2C0312305C317b241D64e7CaB3e52";
 export const stakingContractAddress =
-  "0x8Aa54266892bC0Fb916c504a527C2119489e52b7";
+  "0xC664c007f6D8B8522a3B02d59df269886ACA2c7a";
