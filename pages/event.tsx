441d32e42073e766546e6657c6447ff306eb9296
ZenoUsman <121540289+zenoUsman@users.noreply.github.com>
2023-12-30 19:10:48 +0000

    Gas limit

--- pages/event.tsx
+++ pages/event.tsx
@@ -8,6 +8,7 @@ import { css } from '@emotion/react';
 import { MoonLoader } from 'react-spinners';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 const Event: NextPage = () => {
   const [walletChanged, setWalletChanged] = useState<boolean>(false);
@@ -77,46 +78,96 @@ const Event: NextPage = () => {
     }
   };
 
+  
   const handleClaimTokens = async () => {
     try {
       setLoading(true);
-
+  
       if (contract && address) {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const contractInstance = new ethers.Contract(
           "0xd16144c0EF19eE5382f26dD8FF48D683fa39A87C",
-          ["function claimTokens()"],
+          ["function claimTokens()", "function earnedTokens() view returns (uint256)"],
           signer
         );
-        const transaction = await contractInstance.claimTokens({
-          gasLimit: 500000, // Set a reasonable gas limit for Polygon
-        });
-
-        // Wait for the transaction to be mined
-        await transaction.wait();
-
-        // Show success notification
-        toast.success('Tokens claimed successfully!');
+  
+        // Check the amount of earned tokens
+        const earnedTokensValue = await contractInstance.earnedTokens();
+        const exactValue = 98605;
+  
+        // Additional check: If earnedTokensValue is exactly 98605, set earnedTokens to zero
+        if (earnedTokensValue.eq(exactValue)) {
+          setEarnedTokens(0);
+          toast.info('Tokens value is exactly 98605. Setting earnedTokens to zero.');
+        } else {
+          const tokensValue: number = earnedTokensValue.isZero()
+            ? 0
+            : parseFloat(ethers.utils.formatUnits(earnedTokensValue, 18));
+  
+          // Continue with the claim operation only if tokensValue is greater than or equal to the claim threshold
+          const claimThreshold = 1;
+          if (tokensValue >= claimThreshold) {
+            // Fetch the current gas price from Polygon Gas Station API
+            const gasPriceResponse = await axios.get('https://gasstation.polygon.technology/v2');
+            console.log('Gas Price Response:', gasPriceResponse.data);
+  
+            // Check if the 'fast' property is present in the response and is a valid numeric value
+            const fetchedGasPrice = parseFloat(gasPriceResponse.data?.fast?.maxFee);
+  
+            // Validate that the fetched gas price is a valid number
+            if (isNaN(fetchedGasPrice) || !isFinite(fetchedGasPrice)) {
+              console.error('Invalid gas price format. Gas Price Response:', gasPriceResponse.data);
+              throw new Error('Invalid gas price format');
+            }
+  
+            // Set a fallback gas limit in case of errors
+            const fallbackGasLimit = 1000000;
+  
+            // Set the gas limit based on the current gas price, with a fallback in case of errors
+            const dynamicGasLimit = Math.floor(10000000 / fetchedGasPrice) || fallbackGasLimit;
+  
+            // Call the claimTokens function on the smart contract with the dynamically calculated gas limit
+            const transaction = await contractInstance.claimTokens({
+              gasLimit: dynamicGasLimit,
+            });
+  
+            // Wait for the transaction to be mined
+            await transaction.wait();
+  
+            // Show success notification
+            toast.success('Tokens claimed successfully!');
+          } else {
+            // Show error notification - Not enough tokens to claim
+            toast.error('Error: You do not have enough tokens to claim.');
+          }
+        }
       } else {
-        // Show error notification
+        // Show error notification - Unable to interact with the contract
         toast.error('Error: Unable to claim tokens. Please check your wallet and contract.');
       }
     } catch (error: any) {
-      // Explicitly type 'error' as 'any' or 'Error' to resolve the TypeScript error
-      const typedError: Error = error as Error;
-
+      // Log the entire error object for debugging
+      console.error('Error claiming tokens:', error);
+  
       // Show error notification
-      toast.error(`Error claiming tokens: ${typedError.message}`);
+      toast.error(`Error claiming tokens: ${error.message}`);
     } finally {
       // Hide loading spinner
       setLoading(false);
-
+  
       // Refresh token data after claiming
       fetchAccumulatedTokens();
       fetchEarnedTokens();
     }
   };
+  
+  
+  
+  
+  
+  
+  
 
   useEffect(() => {
     fetchAccumulatedTokens();
